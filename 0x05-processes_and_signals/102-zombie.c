dc02fc60b399aabff6b957ca51cb203dafd291c8
favour adagher <favouradagher@gmail.com>
2023-09-29 04:24:45 -0700

    zombie.c

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-
+#include <stdlib.h>
+/**
+ * infinite_while - infinit while
+ *
+ * Return: 0
+ */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
-
+/**
+ * main - create 5 zombie processe
+ *
+ * Return: void
+ */
 int main(void)
 {
-    int i;
-    pid_t child_pid;
+	pid_t zombie;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Error creating child process");
-            exit(EXIT_FAILURE);
-        }
-
-        if (child_pid == 0)
-        {
-            // Child process
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-    }
-
-    infinite_while(); // Parent process enters an infinite loop
-
-    return (0);
+	for (i = 0; i < 5; i++)
+	{
+		zombie = fork();
+		if (!zombie)
+			return (0);
+		printf("Zombie process created, PID: %d\n", zombie);
+	}
+	infinite_while();
+	return (1);
 }
-
